98e112e758d398963ecad7d26184fd335326a6ca
Martin Bonnin <martin@mbonnin.net>
2022-02-11 10:43:07 +0100

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.apollographql.apollo3").version("3.1.0")
+
+}
+
+android {
+    compileSdkVersion(31)
+
+    defaultConfig {
+        applicationId = "com.example.rocketreserver"
+        minSdkVersion(23)
+        targetSdkVersion(31)
+        versionCode = 1
+        versionName = "1.0"
+    }
+    
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("io.coil-kt:coil:1.4.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation("androidx.paging:paging-runtime-ktx:3.0.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.security:security-crypto:1.1.0-alpha03")
+
+    implementation("com.apollographql.apollo3:apollo-runtime:3.1.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
+
+apollo {
+    packageName.set("com.example.rocketreserver")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.3")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
