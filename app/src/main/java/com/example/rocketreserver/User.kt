9d8153ef4ad1aa0e92f1098a5a1caf9ff017a1a8
Martin Bonnin <martin@mbonnin.net>
2022-02-11 14:45:29 +0100

    remove unused imports

--- app/src/main/java/com/example/rocketreserver/User.kt
+++ app/src/main/java/com/example/rocketreserver/User.kt
@@ -1,10 +1,5 @@
 package com.example.rocketreserver
 
-import android.content.Context
-import android.content.SharedPreferences
-import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKeys
-
 object User {
     var token: String? = null
 }
